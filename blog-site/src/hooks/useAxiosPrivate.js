71677e2d0fe57488cb0a1829f3f47cce9fbfea01
Mavroudis Mantonitsas <m.mantonitsas@gmail.com>
2022-03-23 22:21:53 +0200

    Added the packages and the api interceptor.

---
+++ blog-site/src/hooks/useAxiosPrivate.js
@@ -0,0 +1,26 @@
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
+
+const useAxiosPrivate = () => {
+
+    useEffect(() => {
+
+        const requestIntercept = axiosPrivate.interceptors.request.use(
+            config => {
+                if (!config.headers['Authorization']) {
+                    console.log("here");
+                    config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+                }
+                return config;
+            }, (error) => Promise.reject(error)
+        );
+
+        return () => {
+            axiosPrivate.interceptors.request.eject(requestIntercept);
+        }
+    }, [])
+
+    return axiosPrivate;
+}
+
+export default useAxiosPrivate;
